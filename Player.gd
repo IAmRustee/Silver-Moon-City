69886be238f650a76596b231eaa881482fd9e274
IAmRustee <wewen305@gmail.com>
2026-05-22 21:54:15 +0800

    Added Player Character movement and animations

--- Player.gd
+++ Player.gd
@@ -1,38 +1,32 @@
 extends CharacterBody2D
 
-var score: int = 0
+@onready var animationPlayer: AnimationPlayer = $Sprite2D/AnimationPlayer
 
-@export var speed: int = 100
-@export var jumpForce: int = 1000000
-@export var gravity: int = 800
-
-var grounded: bool = false
-
-@onready var sprite = $Sprite
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -1000.0
+const maxSpeed: int = 70
+const acceleration: int = 5
+const friction: int = 8
 
 func _physics_process(delta: float) -> void:
-	#velocity
-	velocity = Vector2.DOWN * 5
-	move_and_slide()
-	velocity.x = 0
+	var input = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	).normalized()
 	
-	#movement inputs
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.get_action_strength("ui_right") or Input.get_action_strength("ui_left"):
+		animationPlayer.play("side_walk")
+		$Sprite2D.flip_h = true if input.x < 0 else false
+		animationPlayer.speed_scale = (velocity/maxSpeed).distance_to(Vector2.ZERO) + 0.5
+	elif Input.get_action_strength("ui_up"):
+		animationPlayer.play("up_walk")
+		animationPlayer.speed_scale = (velocity/maxSpeed).distance_to(Vector2.ZERO) + 0.5
+	elif Input.get_action_strength("ui_down"): 
+		animationPlayer.play("down_walk")
+		animationPlayer.speed_scale = (velocity/maxSpeed).distance_to(Vector2.ZERO) + 0.5
+	else: 
+		animationPlayer.play("idle front")
+		animationPlayer.speed_scale = 0.75
+	
+	var lerpWeight = delta * (acceleration if input else friction)
+	velocity = lerp(velocity, input * maxSpeed, lerpWeight)
+	
 	move_and_slide()
