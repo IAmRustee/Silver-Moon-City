72b699d4f850a73c526f62d2871a0670e87d677f
IAmRustee <wewen305@gmail.com>
2026-05-25 07:50:21 +0800

    Changes

--- scripts/wooden_sword_craft.gd
+++ scripts/wooden_sword_craft.gd
@@ -1,22 +1,24 @@
 extends Button
 
 @onready var inv: Inv = preload("res://scenes/gui/player_inventory/player_inv.tres")
-@onready var slots: Array = $GridContainer.get_children()
-const wooden_sword = preload("uid://b72gvccoli5m2")
+@onready var slots = $"../../GridContainer".get_children()
 
-
-func _ready() -> void:
-	pass # Replace with function body.
+@export var wood: InventoryItem
+@export var result: InventoryItem
 
 func craftWoodSword():
-	var wood = load("res://items/wood.tres")
 
 	for slot in slots:
-		if slot.get("item") == wood:
-			if slot.amount >= 3:
-				inv.insert(wooden_sword)
-				slot.amount -= 3
-				return
-	
+
+		if slot.get("Inv_Slot") != null:
+
+			if slot.Inv_Slot.InventoryItem == wood:
+
+				if slot.Inv_Slot.amount >= 3:
+
+					inv.insert(result)
+					slot.Inv_Slot.amount -= 3
+					return
+
 func _on_pressed() -> void:
 	craftWoodSword()
